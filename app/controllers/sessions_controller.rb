69cc1534d9a824c95dde1b39514555eeb2d363e3
koliagogol <koliagogol88@gmail.com>
2021-08-04 15:53:45 +0300

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      # Осуществить вход пользователя и перенаправление на страницу профиля.
+    else
+      # Выдать сообщение об ошибке.
+      flash.now[:danger] = 'Invalid email/password combination' # Не совсем верно!
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ get '/about',
 to: 'static_pages#about'
 get '/contact', to: 'static_pages#contact'
 get '/signup' , to: 'users#new'
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
 resources :users
 end
